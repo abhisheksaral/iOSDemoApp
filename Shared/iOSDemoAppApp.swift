9ea04f4e8817a9cbef958df38b72136d89421069
Abhishek Saral <abhisheksaral17@gmail.com>
2021-07-19 05:04:52 -0700

    Initial Commit

---
+++ Shared/iOSDemoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOSDemoAppApp.swift
+//  Shared
+//
+//  Created by Abhishek Saral on 7/19/21.
+//
+
+import SwiftUI
+
+@main
+struct iOSDemoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
